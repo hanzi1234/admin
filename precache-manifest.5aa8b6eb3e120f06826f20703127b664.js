ac199ef77d3f91ef1ae42045a45ad0bec1952962
hanzla <hanzla297@gmail.com>
2019-02-18 22:59:04 +0500

    Updates

---
+++ precache-manifest.5aa8b6eb3e120f06826f20703127b664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc4e5f6d2b097b98da1",
+    "url": "/HANZI/static/js/runtime~main.1fc4e5f6.js"
+  },
+  {
+    "revision": "57affd825cfb25bcb750",
+    "url": "/HANZI/static/js/main.57affd82.chunk.js"
+  },
+  {
+    "revision": "1d855d9bc2991221d00e",
+    "url": "/HANZI/static/js/1.1d855d9b.chunk.js"
+  },
+  {
+    "revision": "57affd825cfb25bcb750",
+    "url": "/HANZI/static/css/main.ba976d8f.chunk.css"
+  },
+  {
+    "revision": "5caf7b642d166bb9cde3c91b9e7a2e32",
+    "url": "/HANZI/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/HANZI/precache-manifest.5aa8b6eb3e120f06826f20703127b664.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/HANZI/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
